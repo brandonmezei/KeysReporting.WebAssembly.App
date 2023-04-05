3f7929676935a73ef6ba1f0f85037b851338b5f0
Brandon Mezei <goobersrus@outlook.com>
2023-04-05 15:35:42 -0400

    Quote Calculator

---
+++ KeysReporting.WebAssembly.App/Client/Pages/Quote/Calculator.razor.cs
@@ -0,0 +1,102 @@
+﻿using KeysReporting.WebAssembly.App.Client.Shared;
+using KeysReporting.WebAssembly.App.Client.Static;
+using KeysReporting.WebAssembly.App.Shared.Quote;
+using Microsoft.AspNetCore.Components;
+
+namespace KeysReporting.WebAssembly.App.Client.Pages.Quote
+{
+    public partial class Calculator
+    {
+
+        public CalculatorDto _calculatorDto = new();
+
+        public string? _buttonClass;
+        public string? _errorMessage;
+        public string? _loadingMessage;
+
+        public int? _removeID;
+        public int _section = 0;
+
+        private async Task HandleYearChange(ChangeEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(e.Value.ToString()))
+                {
+                    _calculatorDto.Year = null;
+                }
+                else
+                {
+                    _calculatorDto.Year = int.Parse(e.Value.ToString());
+                }
+            }
+            catch
+            {
+                _errorMessage = Messages.SomethingWentWrong;
+            }
+
+        }
+
+        private async Task HandleAddCampaign()
+        {
+            _buttonClass = CSSClasses.ButtonSpin;
+            _errorMessage = string.Empty;
+
+            try
+            {
+                var id = 1;
+
+                id = _calculatorDto.CampaignLines.Any()
+                    ? _calculatorDto.CampaignLines.OrderByDescending(x => x.ID).FirstOrDefault().ID.Value + 1
+                    : id;
+
+                _calculatorDto.CampaignLines.Add(new CampaignLine { ID = id });
+            }
+            catch
+            {
+                _errorMessage = Messages.SomethingWentWrong;
+            }
+
+            _loadingMessage = string.Empty;
+            _buttonClass = string.Empty;
+        }
+
+        private async Task RemoveCampaign(int? id)
+        {
+            _buttonClass = CSSClasses.ButtonSpin;
+            _errorMessage = string.Empty;
+
+            if (id.HasValue)
+            {
+                var removeLine = _calculatorDto.CampaignLines.Where(x => x.ID == id).FirstOrDefault();
+
+                if (removeLine != null)
+                    _calculatorDto.CampaignLines.Remove(removeLine);
+
+                _removeID = null;
+
+                if (!_calculatorDto.CampaignLines.Any())
+                    _calculatorDto.CampaignLines.Add(new CampaignLine { ID = 1 });
+            }
+            else
+                _errorMessage = Messages.SomethingWentWrong;
+
+            _loadingMessage = string.Empty;
+            _buttonClass = string.Empty;
+        }
+
+        private void ToggleRemove(int? id)
+        {
+            _removeID = id;
+        }
+
+        private void ToggleSection(int? id)
+        {
+            if (id.HasValue)
+                _section = id.Value;
+
+            if(_section == 1 && !_calculatorDto.CampaignLines.Any())
+                _calculatorDto.CampaignLines.Add(new CampaignLine { ID = 1 });
+        }
+    }
+}
---
+++ KeysReporting.WebAssembly.App/Shared/Quote/CalculatorDto.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace KeysReporting.WebAssembly.App.Shared.Quote
+{
+    public class CalculatorDto
+    {
+        public string? ClientName { get; set; }
+        public int? Year { get; set; }
+
+        public CostBreakDown BreakDown { get; set; } = new();
+
+        public List<CampaignLine> CampaignLines { get; set; } = new();
+
+    }
+
+    public class CostBreakDown
+    {
+        public decimal? SetUpFees { get; set; }
+        public decimal? PhoneAppend { get; set; }
+        public decimal? CostOfAdmin { get; set; }
+    }
+
+    public class CampaignLine
+    {
+        private decimal? _grossNames;
+        private decimal? _oTGResponse;
+        private decimal? _OTGAvg;
+        private decimal? _PACResponse;
+        private decimal? _PACAVGGift;
+        private decimal? _MailResponse;
+        private decimal? _MailAverage;
+
+        private void Recalculate()
+        {
+            //Handle Blanks
+            _grossNames = _grossNames ?? 0;
+            _oTGResponse = _oTGResponse ?? 0;
+            _OTGAvg = _OTGAvg ?? 0;
+            _PACResponse = _PACResponse ?? 0;
+            _PACAVGGift = _PACAVGGift ?? 0;
+            _MailResponse = _MailResponse ?? 0;
+            _MailAverage = _MailAverage ?? 0;
+
+            //Calls
+            CompleteCalls = _grossNames.Value * (decimal)0.5;
+            CallingHours = CompleteCalls.Value / (decimal)5;
+            CampaignCost = CallingHours.Value * (decimal)48;
+
+            //OTG
+            OTGDonors = CompleteCalls * (_oTGResponse / (decimal)100);
+            OTGRevenue = OTGDonors * _OTGAvg;
+
+            //PAC
+            PACGifts = CompleteCalls * (_PACResponse / (decimal)100);
+            PACMonthly = _PACAVGGift * PACGifts;
+            PAC1Year = PACMonthly * (decimal)12;
+
+            //Mail Average
+            MailDonors = CompleteCalls * (_MailResponse / (decimal)100);
+            MailFulfilled = MailDonors * (decimal)0.5;
+            MailRevenue = MailFulfilled * _MailAverage;
+        }
+
+        public int? ID { get; set; }
+        public string? Campaign { get; set; }
+        public string? TargetAudience { get; set; }
+
+        public decimal? GrossNames {
+            get { return _grossNames; }
+            set 
+            {
+                _grossNames = value;
+                Recalculate();          
+            }
+        }
+
+        public decimal? CompleteCalls { get; set; }
+        public decimal? CallingHours { get; set; }
+        public decimal? CampaignCost { get; set; }
+
+        public decimal? OTGResponse { 
+            get { return _oTGResponse; } 
+            set
+            {
+                _oTGResponse = value;
+                Recalculate();
+            }
+        }
+
+        public decimal? OTGDonors { get; set; }
+        public decimal? OTGRevenue { get; set; }
+        public decimal? OTGAvg
+        {
+            get { return _OTGAvg; }
+            set
+            {
+                _OTGAvg = value;
+                Recalculate();
+            }
+        }
+        public decimal? PACResponse 
+        { 
+            get { return _PACResponse; }
+            set
+            {
+                _PACResponse = value;
+                Recalculate();
+            }
+        }
+        public decimal? PACGifts { get; set; }
+        public decimal? PACAVGGift 
+        { 
+            get { return _PACAVGGift; }
+            set
+            {
+                _PACAVGGift = value;
+                Recalculate();
+            }
+        }
+        public decimal? PACMonthly { get; set; }
+        public decimal? PAC1Year { get; set; }
+        public decimal? MailResponse 
+        { 
+            get { return _MailResponse; }
+            set
+            {
+                _MailResponse = value;
+                Recalculate();
+            }
+        }
+        public decimal? MailDonors { get; set; }
+        public decimal? MailFulfilled { get; set; } 
+        public decimal? MailRevenue { get; set; }
+        public decimal? MailAverage 
+        { 
+            get { return _MailAverage; }
+            set
+            {
+                _MailAverage = value;
+                Recalculate();
+            }
+        }
+    }
+}
